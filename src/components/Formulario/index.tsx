bc78b97eb2de39aed72216d03afdb1b15d495e29
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-02 11:28:20 -0300

    commits do alura-studies

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Botao from "../Botao";
+import style from './formulario.module.scss'
+
+class Formulario extends React.Component{
+    render() {
+        return(
+
+            <form className={style.novaTarefa}>
+
+             <div className={style.inputContainer}>
+                <label htmlFor="tarefa">
+                Digite um novo estudo 
+                </label>
+
+                <input type="text" name="tarefa" id="tarefa"
+                placeholder="o que voce deseja estudar?" required/>
+             </div>
+
+             <div className={style.inputContainer}>
+
+             <label htmlFor="tempo">
+                Tempo
+            </label>
+
+            <input type="time" step="1" name="tempo" id="tempo" max="01:30:00" min="00:00:00" required/>
+        
+             </div>
+
+            <Botao/>
+
+            </form>
+        )
+    }
+}
+
+export default Formulario
---
+++ src/pages/App.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Lista from '../components/Listas';
+import Formulario from '../components/Formulario';
+import style from "./App.module.scss"
+
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+      <Formulario/>
+      <Lista/>
+    </div>
+     
+  );
+}
+
+export default App;
