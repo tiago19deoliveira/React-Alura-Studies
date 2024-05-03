1e2fd92348f54934d2f53614d4cab80afd5bc3bf
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-03 11:44:03 -0300

    refactor: refatoração da lista de itens utilizando o conceito de props

---
+++ src/components/Listas/Item/item.tsx
@@ -0,0 +1,12 @@
+import style from "../../components/Listas/lista.module.scss"
+
+
+export default function Item ({tarefa,tempo}: {tarefa:string, tempo:string}){
+    return(
+
+        <li className={style.item}>
+        <h3>{tarefa}</h3>
+       <span>{tempo}</span>     
+       </li>
+    )
+}
