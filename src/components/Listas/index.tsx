77829ad811ec9edd792852998269412a2589dc4c
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-03 11:44:44 -0300

    refactor:refatorando código utilizando o conceito de props e function components

--- src/components/Listas/index.tsx
+++ src/components/Listas/index.tsx
@@ -1,6 +1,7 @@
 
 import React from "react";
 import style from './lista.module.scss'
+import Item from "./Item/item";
 
 function Lista(){
   const tarefas =[{
@@ -22,10 +23,10 @@ function Lista(){
      <h2>Estudos do Dia</h2>
      <ul>
        {tarefas.map((item,index) => (
-            <li key={index} className={style.item}>
-                <h3>{item.tarefa}</h3>
-                <span>{item.tempo}</span>
-            </li>
+       <Item
+       key={index}
+       {...item}
+       />
         ))}</ul> 
   </aside>
 )
