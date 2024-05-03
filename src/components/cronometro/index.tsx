d83232031cc3525d2e4c15f912b6b5e06eba5229
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-03 11:43:15 -0300

    feat:inicio do componente de cronometro

---
+++ src/components/cronometro/index.tsx
@@ -0,0 +1,18 @@
+
+import Botao from '../Botao/index'
+import Button from '../Botao/index'
+
+export default function Cronometro(){
+    <div>
+        <p>
+            EScreve um card e inicie o cronometro 
+        </p>
+    <div>
+    <Botao>
+    começar
+    </Botao>
+
+    </div>
+
+    </div>
+}
