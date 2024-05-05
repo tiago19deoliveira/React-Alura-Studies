bdd71e2a8f66933fa2924e8e254136c5ad03537c
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-05 01:02:10 -0300

    implementando o conceito de use state me diferentes partes do componente

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,18 +3,34 @@ import Botao from "../Botao";
 import style from './formulario.module.scss'
 
 class Formulario extends React.Component{
+
+    adicionarTarefa(evento : React.FormEvent<HTMLFormElement>){
+        evento.preventDefault()
+        console.log("state" ,this.adicionarTarefa)
+    }
+
+
+state={
+    tempo: "",
+    tarefa:""
+}
     render() {
         return(
 
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
 
              <div className={style.inputContainer}>
                 <label htmlFor="tarefa">
                 Digite um novo estudo 
                 </label>
 
-                <input type="text" name="tarefa" id="tarefa"
-                placeholder="o que voce deseja estudar?" required/>
+                <input type="text" 
+                name="tarefa" 
+                id="tarefa"
+                value={this.state.tarefa}
+                onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
+                placeholder="o que voce deseja estudar?" 
+                required/>
              </div>
 
              <div className={style.inputContainer}>
@@ -23,7 +39,15 @@ class Formulario extends React.Component{
                 Tempo
             </label>
 
-            <input type="time" step="1" name="tempo" id="tempo" max="01:30:00" min="00:00:00" required/>
+            <input type="time" 
+            step="1" 
+            name="tempo" 
+            id="tempo" 
+            value={this.state.tempo}
+            onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
+            max="01:30:00"
+            min="00:00:00"
+            required/>
         
              </div>
 
--- src/components/Listas/index.tsx
+++ src/components/Listas/index.tsx
@@ -2,25 +2,27 @@
 import React from "react";
 import style from './lista.module.scss'
 import Item from "./Item/item";
+import {useState} from "react";
 
 function Lista(){
-  const tarefas =[{
-    tarefa: 'React',
-    tempo: '02:00:00',
+ const [tarefas, setTarefas]= useState([{
+  tarefa: 'React',
+  tempo: '02:00:00',
+},{
+  tarefa: 'JavaScript',
+  tempo: '01:00:00'
+},{
+  tarefa: 'TypeScript',
+  tempo:'04:00:00'
   },{
-    tarefa: 'JavaScript',
-    tempo: '01:00:00'
-  },{
-    tarefa: 'TypeScript',
-    tempo:'04:00:00'
-    },{
-        tarefa: 'Banco De Dados',
-        tempo : '10:00:00'
-    }]
-
+      tarefa: 'Banco De Dados',
+      tempo : '10:00:00'
+  }])
+ 
  return (
     <aside className={style.listaTarefas}>
-     <h2>Estudos do Dia</h2>
+     <h2 onClick={()=>{setTarefas([...tarefas, {tarefa:"Estudar País", tempo:"03:00:00"}])}
+     } >Estudos do Dia</h2>
      <ul>
        {tarefas.map((item,index) => (
        <Item
