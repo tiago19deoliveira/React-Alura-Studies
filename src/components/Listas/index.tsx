72d9ec108531b983f56d37e4da7c399e0ea15d3e
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-05 13:50:36 -0300

    refactor: importando o array de ITarefas para referenciar na lista

--- src/components/Listas/index.tsx
+++ src/components/Listas/index.tsx
@@ -2,33 +2,19 @@
 import React from "react";
 import style from './lista.module.scss'
 import Item from "./Item/item";
-import {useState} from "react";
 
-function Lista(){
- const [tarefas, setTarefas]= useState([{
-  tarefa: 'React',
-  tempo: '02:00:00',
-},{
-  tarefa: 'JavaScript',
-  tempo: '01:00:00'
-},{
-  tarefa: 'TypeScript',
-  tempo:'04:00:00'
-  },{
-      tarefa: 'Banco De Dados',
-      tempo : '10:00:00'
-  }])
- 
+import { ITarefa } from "../../types/tarefa";
+
+function Lista({tarefas}: {tarefas: ITarefa[]}){
  return (
     <aside className={style.listaTarefas}>
-     <h2 onClick={()=>{setTarefas([...tarefas, {tarefa:"Estudar País", tempo:"03:00:00"}])}
-     } >Estudos do Dia</h2>
+     <h2 
+      >Estudos do Dia</h2>
      <ul>
        {tarefas.map((item,index) => (
        <Item
-       key={index}
-       {...item}
-       />
+           key={index}
+           {...item}       />
         ))}</ul> 
   </aside>
 )
