97ec51d4e38416d0046642a1af2fa4f04e5f219f
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-05 14:36:53 -0300

    refactor: refatorando código utiliznado novos tipos adicionados a interface ITarefa

--- src/components/Listas/Item/item.tsx
+++ src/components/Listas/Item/item.tsx
@@ -1,6 +1,9 @@
+import { ITarefa } from '../../../types/tarefa'
 import style from '../lista.module.scss'
 
-export default function Item ({tarefa,tempo}: {tarefa:string, tempo:string}){
+export default function Item ({tarefa,tempo,selecionado,completado,id}: ITarefa){
+console.log("item atual:",{ tarefa,tempo,selecionado,completado,id} )
+
     return(
 
         <li className={style.item}>
