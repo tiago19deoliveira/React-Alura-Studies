2d959296da99cfbe424e93531c4430814a6a1622
tiago19deoliveira <tiago18deoliveira@gmail.com>
2024-05-05 22:44:08 -0300

    configurando o timer de tarefas, utilizando o conceito de useeffects

--- src/components/cronometro/index.tsx
+++ src/components/cronometro/index.tsx
@@ -1,14 +1,29 @@
-
 import Botao from '../Botao'
 import Relogio  from './Relogio'
 import style from './Cronometro.module.scss'
+import { tempoParaSegundos } from '../../common/utils/time'
+import { ITarefa } from '../../types/tarefa'
+import { useEffect, useState } from 'react'
 
-export default function Cronometro(){
+interface Props{
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({selecionado}:Props){
+    const [tempo, setTempo]= useState<number>()
+
+     useEffect(()=> {
+     if(selecionado?.tempo)
+    {
+     setTempo(tempoParaSegundos(selecionado.tempo))
+    }
+     },[selecionado])
     return(
     <div className={style.cronometro}>
         <p className={style.titulo}>
             EScreve um card e inicie o cronometro 
         </p>
+        Tempo: {tempo}
     <div className={style.relogioWrapper}>
      <Relogio/>
     </div>
